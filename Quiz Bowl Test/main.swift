f0602fc4ced17a247b2dd1ffc16d94619e404e16
MUNNS MATTHEW <818222@ehs-s-itc-25>
2015-12-11 10:53:59 -0600

    Initial Commit

---
+++ Quiz Bowl Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Quiz Bowl Test
+//
+//  Created by MUNNS, MATTHEW on 12/11/15.
+//  Copyright © 2015 MUNNS, MATTHEW. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
